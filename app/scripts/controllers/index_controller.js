1103abc9702db3d790fc94efb0eeded64a105b40
Glavin Wiechert <glavin.wiechert@gmail.com>
2014-05-14 04:00:40 -0300

    Add test for pushing new fragment records into existing model record.

--- app/scripts/controllers/index_controller.js
+++ app/scripts/controllers/index_controller.js
@@ -1,3 +1,26 @@
 App.IndexController = Ember.ArrayController.extend({
+  actions: {
+      addAddress: function(person) {
 
+          var a = {
+            "street": "1 Sky Cell",
+            "city": "Eyre",
+            "region": "Vale of Arryn",
+            "country": "Westeros"
+          };
+
+          var frag = this.store.createFragment('address', a);
+          person.get('addresses').pushObject(frag);
+          person.save();
+          console.log(person);
+      },
+      addTitle: function(person) {
+
+          var t = "Awesome";
+
+          person.get('titles').pushObject(t);
+          person.save();
+          console.log(person);
+      }
+  }
 });
