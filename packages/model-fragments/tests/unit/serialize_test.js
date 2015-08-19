f1f27690b4c7583194f271d880315c63fcf64e84
Steven Lindberg <stevenjohnlindberg@gmail.com>
2015-08-19 13:43:09 -0700

    Fixed warnings in tests

--- packages/model-fragments/tests/unit/serialize_test.js
+++ packages/model-fragments/tests/unit/serialize_test.js
@@ -44,8 +44,8 @@ test("fragment properties are snapshotted as normal attributes on the owner reco
   expect(8);
 
   Person.reopen({
-    houses   : DS.hasManyFragments('house'),
-    children : DS.hasManyFragments()
+    houses   : DS.hasManyFragments('house', { defaultValue: [] }),
+    children : DS.hasManyFragments(null, { defaultValue: [] })
   });
 
   var person = {
@@ -129,7 +129,7 @@ test("fragment properties are serialized as normal attributes using their own se
 
 test("serializing a fragment array creates a new array with contents the result of serializing each fragment", function() {
   Person.reopen({
-    names: DS.hasManyFragments('name'),
+    names: DS.hasManyFragments('name', { defaultValue: [] }),
   });
 
   var names = [
@@ -166,8 +166,8 @@ test("serializing a fragment array creates a new array with contents the result 
 
 test("normalizing data can handle `null` fragment values", function() {
   Person.reopen({
-    houses   : DS.hasManyFragments('house'),
-    children : DS.hasManyFragments()
+    houses   : DS.hasManyFragments('house', { defaultValue: null }),
+    children : DS.hasManyFragments(null, { defaultValue: null })
   });
 
   var normalized = store.normalize('person', {
