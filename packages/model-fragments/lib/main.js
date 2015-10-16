4323ae72340ddc6013c22692906fb867d6aab3f9
Steven Lindberg <stevenjohnlindberg@gmail.com>
2015-10-15 20:20:13 -0700

    Removed deprecated APIs

--- packages/model-fragments/lib/main.js
+++ packages/model-fragments/lib/main.js
@@ -1,7 +1,4 @@
-/* jshint unused: false */
 import Ember from 'ember';
-import DS from 'ember-data';
-import { Store, Model } from './fragments/ext';
 import Fragment from './fragments/fragment';
 import FragmentArray from './fragments/array/fragment';
 import FragmentTransform from './fragments/transforms/fragment';
@@ -14,59 +11,6 @@ import {
   array,
   fragmentOwner
 } from './fragments/attributes';
-
-// Export classes to DS namespace for backwards compatibility
-DS.ModelFragment = Fragment;
-DS.FragmentArray = FragmentArray;
-DS.FragmentTransform = FragmentTransform;
-DS.FragmentArrayTransform = FragmentArrayTransform;
-DS.ArrayTransform = ArrayTransform;
-
-/**
-  `DS.hasOneFragment` has been deprecated in favor of `MF.fragment`.
-
-  @namespace DS
-  @method hasOneFragment
-  @deprecated
-*/
-DS.hasOneFragment = function hasOneFragmentDeprecation(modelName, options) {
-  Ember.deprecate("The `DS.hasOneFragment` property has been deprecated in favor of `MF.fragment`");
-
-  return fragment(modelName, options);
-};
-
-/**
-  `DS.hasManyFragments` has been deprecated in favor of `MF.fragmentArray`.
-
-  @namespace DS
-  @method hasManyFragments
-  @deprecated
-*/
-DS.hasManyFragments = function hasManyFragmentsDeprecation(modelName, options) {
-  // If a modelName is not given, it implies an array of primitives
-  if (Ember.typeOf(modelName) !== 'string') {
-    Ember.deprecate("The `DS.hasManyFragments` property without a model name has been deprecated in favor of `MF.array`");
-
-    return array(null, options);
-  }
-
-  Ember.deprecate("The `DS.hasManyFragments` property has been deprecated in favor of `MF.fragmentArray`");
-
-  return fragmentArray(modelName, options);
-};
-
-/**
-  `DS.fragmentOwner` has been deprecated in favor of `MF.fragmentOwner`.
-
-  @namespace DS
-  @method fragmentOwner
-  @deprecated
-*/
-DS.fragmentOwner = function fragmentOwnerDeprecation() {
-  Ember.deprecate("The `DS.fragmentOwner` property has been deprecated in favor of `MF.fragmentOwner`");
-
-  return fragmentOwner();
-};
 
 /**
   Ember Data Model Fragments
