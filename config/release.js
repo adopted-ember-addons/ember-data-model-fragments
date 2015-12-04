2a87ccbb1c826f3df7f24cd2581ae677fe379648
Steven Lindberg <stevenjohnlindberg@gmail.com>
2015-12-04 09:48:46 -0800

    Added minor version CI restriction update to release config

--- config/release.js
+++ config/release.js
@@ -3,24 +3,56 @@
 var BuildTask = require('ember-cli/lib/tasks/build');
 var RSVP = require('rsvp');
 var publisher = require('publish');
+var fs = require('fs');
+var path = require('path');
 
 // Create promise friendly versions of the methods we want to use
 var start = RSVP.denodeify(publisher.start);
 var publish = RSVP.denodeify(publisher.publish);
+var readFile = RSVP.denodeify(fs.readFile);
+var writeFile = RSVP.denodeify(fs.writeFile);
+
+function getMinorVersion(version) {
+  var match = version.match(/v(.+)\..+$/);
+  return match[1];
+}
+
+function replaceInFile(rootPath, filePath, find, replace) {
+  var fullPath = path.join(rootPath, filePath);
+  var findRegexp = new RegExp(find, 'g');
+
+  return readFile(fullPath, 'utf8')
+    .then(function(content) {
+      var replacedContent = content.replace(findRegexp, replace);
+      return writeFile(fullPath, replacedContent, 'utf8');
+    });
+}
 
 module.exports = {
-  // Build the project in the production environment, outputting to dist/
-  beforeCommit: function(project) {
+  beforeCommit: function(project, versions) {
+    var promises = [];
+
+    // Update minor version restriction in CI
+    var latest = '~' + getMinorVersion(versions.latest);
+    var next = '~' + getMinorVersion(versions.next);
+
+    if (latest !== next) {
+      promises.push(replaceInFile(project.root, '.travis.yml', latest, next));
+    }
+
+    // Build the project in the production environment, outputting to dist/
     var task = new BuildTask({
       project: project,
       ui: project.ui,
       analytics: project.cli.analytics
     });
 
-    return task.run({
+    promises.push(task.run({
       environment: 'production',
       outputPath: 'dist/'
-    });
+    }));
+
+    return RSVP.all(promises);
   },
 
   // Publish the new release to NPM after a successful push
