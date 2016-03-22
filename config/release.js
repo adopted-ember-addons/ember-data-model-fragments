bf1113b1a9a34837bd58662dd5fa29fe6993467c
Steven Lindberg <stevenjohnlindberg@gmail.com>
2016-03-21 17:58:27 -0700

    Removed release hook for updating ember minor version in travis
    This needs to be done before the release happens, so that PRs against
    the new version will work in CI

--- config/release.js
+++ config/release.js
@@ -3,43 +3,13 @@
 var BuildTask = require('ember-cli/lib/tasks/build');
 var RSVP = require('rsvp');
 var publisher = require('publish');
-var fs = require('fs');
-var path = require('path');
 
 // Create promise friendly versions of the methods we want to use
 var start = RSVP.denodeify(publisher.start);
 var publish = RSVP.denodeify(publisher.publish);
-var readFile = RSVP.denodeify(fs.readFile);
-var writeFile = RSVP.denodeify(fs.writeFile);
-
-function getMinorVersion(version) {
-  var match = version.match(/v(.+)\..+$/);
-  return match[1];
-}
-
-function replaceInFile(rootPath, filePath, find, replace) {
-  var fullPath = path.join(rootPath, filePath);
-  var findRegexp = new RegExp(find, 'g');
-
-  return readFile(fullPath, 'utf8')
-    .then(function(content) {
-      var replacedContent = content.replace(findRegexp, replace);
-      return writeFile(fullPath, replacedContent, 'utf8');
-    });
-}
 
 module.exports = {
-  beforeCommit: function(project, versions) {
-    var promises = [];
-
-    // Update minor version restriction in CI
-    var latest = '~' + getMinorVersion(versions.latest);
-    var next = '~' + getMinorVersion(versions.next);
-
-    if (latest !== next) {
-      promises.push(replaceInFile(project.root, '.travis.yml', latest, next));
-    }
-
+  beforeCommit: function(project) {
     // Build the project in the production environment, outputting to dist/
     var task = new BuildTask({
       project: project,
@@ -47,12 +17,10 @@ module.exports = {
       analytics: project.cli.analytics
     });
 
-    promises.push(task.run({
+    return task.run({
       environment: 'production',
       outputPath: 'dist/'
     }));
-
-    return RSVP.all(promises);
   },
 
   // Publish the new release to NPM after a successful push
