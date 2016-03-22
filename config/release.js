18bfc48f08a807691d345da6ff8062c781387fc3
Steven Lindberg <stevenjohnlindberg@gmail.com>
2016-03-21 18:00:22 -0700

    Updated ember-cli-release version for publish support

--- config/release.js
+++ config/release.js
@@ -1,14 +1,10 @@
 /* jshint node:true */
 
 var BuildTask = require('ember-cli/lib/tasks/build');
-var RSVP = require('rsvp');
-var publisher = require('publish');
-
-// Create promise friendly versions of the methods we want to use
-var start = RSVP.denodeify(publisher.start);
-var publish = RSVP.denodeify(publisher.publish);
 
 module.exports = {
+  publish: true,
+
   // Build the project in the production environment, outputting to dist/
   beforeCommit: function(project) {
     var task = new BuildTask({
@@ -20,13 +16,6 @@ module.exports = {
     return task.run({
       environment: 'production',
       outputPath: 'dist/'
-    });
-  },
-
-  // Publish the new release to NPM after a successful push
-  afterPush: function() {
-    return start().then(function() {
-      return publish({});
     });
   }
 };
