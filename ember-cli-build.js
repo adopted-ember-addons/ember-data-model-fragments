9cd1e5066289f9262fc251b60138ecb53e2cfe04
Jacob Jewell <jacobjewell@eflexsystems.com>
2016-03-22 20:45:16 -0400

    restore version number

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -1,6 +1,8 @@
 /*jshint node:true*/
 /* global require, module */
 var EmberAddon = require('ember-cli/lib/broccoli/ember-addon');
+var fs         = require('fs');
+var replace    = require('broccoli-replace')
 
 module.exports = function(defaults) {
   var app = new EmberAddon(defaults, {
@@ -14,5 +16,21 @@ module.exports = function(defaults) {
     behave. You most likely want to be modifying `./index.js` or app's build file
   */
 
-  return app.toTree();
+  var compiled = app.toTree();
+  compiled = versionStamp(compiled);
+  return compiled;
 };
+
+function versionStamp(tree) {
+  // The version in package.json can be changed, so this can't be `required()`
+  // directly, and the git repo version is similarly unreliable
+  var version = JSON.parse(fs.readFileSync('./package.json')).version;
+
+  return replace(tree, {
+    files: ['**/*'],
+    patterns: [{
+      match: /VERSION_STRING_PLACEHOLDER/g,
+      replacement: version
+    }]
+  });
+}
