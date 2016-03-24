595b3e12793835ac6d424649b8f78b69b7a4c72d
Wesley Workman <wworkman@batterii.com>
2016-03-24 19:41:25 -0400

    Configuring ember-try install to use lockstep versions of ember and ember-data. You'll see an addition of `npm run install-and-ember-test`. This is to workaround what I believe is a bug with ember-try. I'm going to follow up with a bug in that repo.

--- config/ember-try.js
+++ config/ember-try.js
@@ -7,49 +7,89 @@ module.exports = {
         dependencies: { }
       }
     },
+
     {
-      name: 'ember-1-13',
+      name: 'ember-2-3',
+      command: 'npm run install-and-ember-test',
       bower: {
-        dependencies: {
-          'ember': '~1.13.0'
+        devDependencies: {
+          'ember': '2.3.1'
         },
         resolutions: {
-          'ember': '~1.13.0'
+          'ember': '2.3.1'
+        }
+      },
+      npm: {
+        devDependencies: {
+          'ember-data': '2.3.3'
+        },
+        resolutions: {
+          'ember-data': '2.3.3'
         }
       }
     },
+
     {
       name: 'ember-release',
+      command: 'npm run install-and-ember-test',
       bower: {
-        dependencies: {
+        devDependencies: {
           'ember': 'components/ember#release'
         },
         resolutions: {
           'ember': 'release'
         }
+      },
+      npm: {
+        devDependencies: {
+          'ember-data': 'emberjs/data#release'
+        },
+        resolutions: {
+          'ember-data': 'release'
+        }
       }
     },
+
     {
       name: 'ember-beta',
+      command: 'npm run install-and-ember-test',
       allowedToFail: true,
       bower: {
-        dependencies: {
+        devDependencies: {
           'ember': 'components/ember#beta'
         },
         resolutions: {
           'ember': 'beta'
         }
+      },
+      npm: {
+        devDependencies: {
+          'ember-data': 'emberjs/data#beta'
+        },
+        resolutions: {
+          'ember-data': 'beta'
+        }
       }
     },
+
     {
       name: 'ember-canary',
+      command: 'npm run install-and-ember-test',
       allowedToFail: true,
       bower: {
-        dependencies: {
+        devDependencies: {
           'ember': 'components/ember#canary'
         },
         resolutions: {
           'ember': 'canary'
+        }
+      },
+      npm: {
+        devDependencies: {
+          'ember-data': 'emberjs/data#master'
+        },
+        resolutions: {
+          'ember-data': 'master'
         }
       }
     }
