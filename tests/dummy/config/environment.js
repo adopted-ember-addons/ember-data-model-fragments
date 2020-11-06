266591319e23c7d6f12f5072f061e7de99bb5f90
Pat O'Callaghan <pat@intercom.io>
2020-11-06 10:11:40 +0000

    Upgrade to ember 3.20 LTS

--- tests/dummy/config/environment.js
+++ tests/dummy/config/environment.js
@@ -10,7 +10,11 @@ module.exports = function(environment) {
       EXTEND_PROTOTYPES: false,
       FEATURES: {
         // Here you can enable experimental features on an ember canary build
-        // e.g. 'with-controller': true
+        // e.g. EMBER_NATIVE_DECORATOR_SUPPORT: true
+      },
+      EXTEND_PROTOTYPES: {
+        // Prevent Ember Data from overriding Date.parse.
+        Date: false
       }
     },
 
