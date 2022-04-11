776620ec4736e032208840393a26419aafb86ca4
Charles Fries <charliefries@icloud.com>
2022-04-11 13:07:53 -0700

    Remove bowerDependencies warning

--- index.js
+++ index.js
@@ -7,22 +7,6 @@ const calculateCacheKeyForTree = require('calculate-cache-key-for-tree');
 module.exports = {
   name: require('./package').name,
 
-  init() {
-    this._super.init && this._super.init.apply(this, arguments);
-
-    let bowerDeps = this.project.bowerDependencies();
-
-    if (bowerDeps['ember-data-model-fragments']) {
-      let message = 'Please remove `ember-data-model-fragments` from `bower.json`, it is no longer used as of v2.3.0';
-
-      if (this.ui.writeWarnLine) {
-        this.ui.writeWarnLine(message);
-      } else {
-        this.ui.writeLine(message);
-      }
-    }
-  },
-
   treeForAddon(tree) {
     let versioned = merge([version(), tree]);
 
