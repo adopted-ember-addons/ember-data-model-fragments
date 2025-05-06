35ca7bc8995846477ab322810d1d747408c09fa9
Robbie Wagner <rwwagner90@gmail.com>
2025-05-06 14:51:16 -0400

    Remove unused licenses (#492)
    * Remove unused licenses
    
    * Force specific embroider versions

--- config/license.js
+++
@@ -1,7 +0,0 @@
-/* !
- * @overview  Ember Data Model Fragments
- * @copyright Copyright 2015 Lytics Inc. and contributors
- * @license   Licensed under MIT license
- *            See https://raw.githubusercontent.com/lytics/ember-data-model-fragments/master/LICENSE
- * @version   VERSION_STRING_PLACEHOLDER
- */
--- config/release.js
+++
@@ -1,5 +0,0 @@
-/* eslint-env node */
-
-module.exports = {
-  publish: true,
-};
