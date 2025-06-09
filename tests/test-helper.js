2b793a5961c1f5d760b350e54894c76729665f29
Robert Wagner <rwwagner90@gmail.com>
2025-06-09 15:31:29 -0400

    Update test-helper.js

--- tests/test-helper.js
+++ tests/test-helper.js
@@ -3,12 +3,14 @@ import config from 'dummy/config/environment';
 import * as QUnit from 'qunit';
 import { setApplication } from '@ember/test-helpers';
 import { setup } from 'qunit-dom';
+import { setup as setupCustomAssertions } from './helpers/assertion';
 import { loadTests } from 'ember-qunit/test-loader';
 import { start, setupEmberOnerrorValidation } from 'ember-qunit';
 
 setApplication(Application.create(config.APP));
 
 setup(QUnit.assert);
+setupCustomAssertions(QUnit.assert);
 setupEmberOnerrorValidation();
 loadTests();
 start();
