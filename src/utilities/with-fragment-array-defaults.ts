da359fe95dba4f8622167e68bab01d9c1d478c01
Robbie Wagner <robbie.wagner@hashicorp.com>
2025-06-25 11:14:54 -0400

    Add schema utills for fragment and fragment-array

---
+++ src/utilities/with-fragment-array-defaults.ts
@@ -0,0 +1,18 @@
+import { singularize } from 'ember-inflector';
+/**
+ * Used as a helper to setup the relevant parts of a fragment-array
+ * schema and add extensions etc.
+ *
+ * @param fragmentArrayName The name of the fragment-array
+ * @returns The schema for a fragment-array
+ */
+export function withFragmentArrayDefaults(fragmentArrayName: string) {
+  return {
+    kind: 'schema-array',
+    type: `fragment:${singularize(fragmentArrayName)}`,
+    name: fragmentArrayName,
+    options: {
+      arrayExtensions: ['ember-object', 'ember-array-like', 'fragment-array'],
+    },
+  };
+}
--- src/utilities/with-fragment-defaults.ts
+++ src/utilities/with-fragment-defaults.ts
@@ -1,9 +1,17 @@
-import { withDefaults } from '@ember-data/model/migration-support';
-import type { WithPartial } from '@warp-drive/core-types/utils';
-import type { LegacyResourceSchema } from '@warp-drive/core-types/schema/fields';
-
-export function withFragmentDefaults(
-  schema: WithPartial<LegacyResourceSchema, 'legacy' | 'identity'>,
-) {
-  return withDefaults({ ...schema });
+/**
+ * Used as a helper to setup the relevant parts of a fragment schema
+ * and add extensions etc.
+ *
+ * @param fragmentName The name of the fragment
+ * @returns The schema for a fragment
+ */
+export function withFragmentDefaults(fragmentName: string) {
+  return {
+    kind: 'schema-object',
+    type: `fragment:${fragmentName}`,
+    name: fragmentName,
+    options: {
+      objectExtensions: ['ember-object', 'fragment'],
+    },
+  };
 }
