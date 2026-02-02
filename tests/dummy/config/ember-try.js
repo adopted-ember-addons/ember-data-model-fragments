1cd0e9f0f444094370bf6c4be037e542696aa9b0
Robbie Wagner <robbie.wagner@hashicorp.com>
2026-02-02 13:48:44 -0500

    Test ember-data 5.3 and 5.8

--- tests/dummy/config/ember-try.js
+++ tests/dummy/config/ember-try.js
@@ -56,6 +56,24 @@ module.exports = async function () {
           },
         },
       },
+      {
+        name: 'ember-data-5.3',
+        npm: {
+          devDependencies: {
+            'ember-data': '~5.3.0',
+            '@ember-data/json-api': '~5.3.0',
+          },
+        },
+      },
+      {
+        name: 'ember-data-5.8',
+        npm: {
+          devDependencies: {
+            'ember-data': '~5.8.0',
+            '@ember-data/json-api': '~5.8.0',
+          },
+        },
+      },
       embroiderSafe(),
       embroiderOptimized(),
     ],
