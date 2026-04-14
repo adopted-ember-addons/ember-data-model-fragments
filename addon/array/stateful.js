3ae002dc85b98c5f34ef94cba1a8a42b2c7009a5
Robbie Wagner <robbie.wagner@hashicorp.com>
2026-04-14 14:49:22 -0400

    Update stateful.js

--- addon/array/stateful.js
+++ addon/array/stateful.js
@@ -2,7 +2,6 @@ import EmberObject, { get } from '@ember/object';
 import { isArray } from '@ember/array';
 import MutableArray from '@ember/array/mutable';
 import { assert } from '@ember/debug';
-import { diffArray } from '@ember-data/model/-private';
 import { copy } from '../util/copy';
 
 /**
